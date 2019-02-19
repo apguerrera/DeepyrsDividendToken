f6a39826275feef8cfb5b4fa70cccb093de71353
Adrian Guerrera <adrianguerrera@gmail.com>
2019-02-20 03:57:13 +1100

    Removed totalEthPoints variable

--- contracts/DeepyrsDividendETHToken.sol
+++ contracts/DeepyrsDividendETHToken.sol
@@ -119,13 +119,10 @@ contract DividendToken is IERC20, Owned {
         uint256 balance;
         uint256 lastDivPoints;
         uint256 lastEthPoints;
-
     }
     mapping(address => Account) public accounts;
 
-    mapping(uint256 => address) public dividendTokenIndex;
     mapping(address => uint256) public totalDividendPoints;
-    uint256 public totalEthPoints;
     mapping(address => uint256) public unclaimedDividends;
     mapping(address => mapping(address => uint256)) public unclaimedDividendByAccount;  // [account][token]
 
@@ -316,34 +313,32 @@ contract DividendToken is IERC20, Owned {
     //------------------------------------------------------------------------
     // Dividends
     //------------------------------------------------------------------------
-
     function dividendsOwing(address _account, address _token) external view returns(uint256) {
         _dividendsOwing(_account, _token);
     }
 
     function _dividendsOwing(address _account, address _token) internal view returns(uint256) {
-        uint256 newDividendPoints;
+        uint256 lastPoints;
         if (_token == address(0x0)) {
-              newDividendPoints = totalEthPoints.sub(accounts[_account].lastEthPoints);
+              lastPoints = accounts[_account].lastEthPoints;
         } else if (_token == dividendTokenAddress) {
-              newDividendPoints = totalDividendPoints[_account].sub(accounts[_account].lastDivPoints);
-        } else {
-              return 0;
+              lastPoints = accounts[_account].lastDivPoints;
         }
+        uint256 newDividendPoints = totalDividendPoints[_token].sub(lastPoints);
         return (accounts[_account].balance * newDividendPoints) / pointMultiplier;
     }
 
     //------------------------------------------------------------------------
     // Dividends: Token Transfers
     //------------------------------------------------------------------------
-
      function _updateAccount(address _account) internal {
-        if (accounts[_account].lastDivPoints < totalDividendPoints[_account]) {
+        if (accounts[_account].lastDivPoints < totalDividendPoints[dividendTokenAddress]) {
               _updateAccountByToken(_account,dividendTokenAddress);
         }
-        if (accounts[_account].lastEthPoints < totalEthPoints) {
+        if (accounts[_account].lastEthPoints < totalDividendPoints[address(0x0)]) {
               _updateAccountByToken(_account,address(0x0));
-        }    }
+        }
+     }
 
     function _updateAccountByToken(address _account, address _token) internal {
         uint256 _owing = _dividendsOwing(_account, _token);
@@ -355,7 +350,6 @@ contract DividendToken is IERC20, Owned {
     //------------------------------------------------------------------------
     // Dividends: Token Deposits
     //------------------------------------------------------------------------
-
     function approveTokenDividend(uint256 _amount) external  {
         require(_amount > 0);
         require(IERC20(dividendTokenAddress).approve(this, _amount));
@@ -370,16 +364,14 @@ contract DividendToken is IERC20, Owned {
 
     function _depositDividends(uint256 _amount, address _token) internal {
         // Convert deposit into points
-        unclaimedDividends[_token] += (_amount * pointMultiplier ) / totalSupply();
+        totalDividendPoints[_token] += (_amount * pointMultiplier ) / totalSupply();
         unclaimedDividends[_token] += _amount;
         emit DividendReceived(now, msg.sender, _amount);
     }
 
-
     //------------------------------------------------------------------------
     // Dividends: Claim accrued dividends
     //------------------------------------------------------------------------
-
     function withdrawlDividends () public  {
         _updateAccount(msg.sender);
         if (unclaimedDividendByAccount[msg.sender][dividendTokenAddress]>0) {
@@ -405,14 +397,12 @@ contract DividendToken is IERC20, Owned {
              // require(transfer(_account, _amount));
         } else if (_token == dividendTokenAddress) {
              // require(IERC20(_token).transfer(_account, _amount));
-
         }
     }
 
     //------------------------------------------------------------------------
     // Dividends: Helper functions
     //------------------------------------------------------------------------
-
     function setDividendTokenAddress (address _token) public onlyOwner {
         require(_token != address(0));
         dividendTokenAddress = IERC20(_token);
@@ -422,7 +412,6 @@ contract DividendToken is IERC20, Owned {
         dividendTokenAddress = IERC20(0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359);
         _depositDividends(msg.value,address(0x0));
     }
-
 
     // ------------------------------------------------------------------------
     // Accept ETH deposits as dividends
